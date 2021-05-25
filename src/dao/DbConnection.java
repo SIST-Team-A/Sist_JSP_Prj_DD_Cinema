bde9c3558026a05f28cb34a884dd5d600055b8ad
oldsalao <bucktoothvillain@gmail.com>
2021-05-25 17:42:30 +0900

    예매 VO DAO 추가 및 import 수정

---
+++ src/dao/DbConnection.java
@@ -0,0 +1,55 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * DBCP를 사용하여 DBConnection을 얻는 일.
+ * @author jin
+ */
+public class DbConnection {
+	private static DbConnection dc;
+	
+	private DbConnection() {
+		
+	}//DbConnection
+	
+	public static DbConnection getInstance() {
+		if(dc == null) {
+			dc = new DbConnection();
+		}//end if
+		return dc;
+	}//getInstance
+	
+	
+	public Connection getConn() throws SQLException{
+		Connection con = null;
+		
+		try {
+		//1. JNDI사용객체 생성
+		Context ctx = new InitialContext();
+		//2. DBCP에서 DB와 연결하고 있는 객체 얻기
+		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/dbcp");
+		//3. DBCP에서 연결하고 있는 객체로 부터 Connection 얻기
+		con = ds.getConnection();
+		}catch(NamingException	ne) {
+			ne.printStackTrace();
+		}//end try~catch
+		
+		return con;
+	}//getConn
+	
+	public void dbClose(Connection con,PreparedStatement pstmt, ResultSet rs) throws SQLException{
+		if( rs != null) { rs.close();}
+		if( pstmt != null) { pstmt.close();}
+		if( con != null) { con.close();}
+	}//dbClose
+	
+}//class
---
+++ src/vo/MovieListVO.java
@@ -0,0 +1,33 @@
+package vo;
+
+public class MovieListVO {
+	private String mvNo, mvTitle;
+
+	public MovieListVO() {
+		super();
+	}
+
+	public MovieListVO( String mvTitle,String mvNo) {
+		super();
+		this.mvNo = mvNo;
+		this.mvTitle = mvTitle;
+	}
+
+	public String getMvNo() {
+		return mvNo;
+	}
+
+	public void setMvNo(String mvNo) {
+		this.mvNo = mvNo;
+	}
+
+	public String getMvTitle() {
+		return mvTitle;
+	}
+
+	public void setMvTitle(String mvTitle) {
+		this.mvTitle = mvTitle;
+	}
+	
+	
+}
